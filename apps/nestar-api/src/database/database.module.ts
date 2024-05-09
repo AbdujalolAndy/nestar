96aa84fb9c9292391403abcf4579e38057351b18
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-05-10 03:34:07 +0900

    fix: Connect MongoDB server

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from "@nestjs/mongoose";
+import { Connection } from "mongoose"
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+    imports: [MongooseModule.forRootAsync({
+        useFactory: () => ({
+            uri: process.env.NODE_ENV === "production" ? process.env.MONGODB_PROD : process.env.MONGODB_DEV
+        })
+    })],
+    exports: [MongooseModule],
+})
+export class DatabaseModule {
+    constructor(@InjectConnection() private readonly connection: Connection) {
+        if (connection.readyState === 1) {
+            console.log(`MONGODB is connected into ${process.env.NODE_ENV === "production" ? "production db" : "development db"}`)
+        } else {
+            console.log("DB is not connected!")
+        }
+    }
+
+
+}
