4f535ffde99a98b683fede701097fcf3a8e74add
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-05-12 04:37:22 +0900

    feat: Validation Pipe Integrate validation pipes logic

--- apps/nestar-api/src/schemas/Member.model.ts
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -27,72 +27,72 @@ const MemberSchema = new Schema({
         index: { unique: true, sparse: true },
         required: true
     },
-    memberPassword:{
-        type:String,
-        select:false,
-        required:true,
+    memberPassword: {
+        type: String,
+        select: false,
+        required: true,
     },
-    memberFullName:{
-        type:String
+    memberFullName: {
+        type: String
     },
-    memberImage:{
-        type:String,
-        default:""
+    memberImage: {
+        type: String,
+        default: ""
     },
-    memberAddress:{
-        type:String,
-    }, 
-    memberDesc:{
-        type:String,
+    memberAddress: {
+        type: String,
     },
-    memberProperties:{
-        type:Number,
-        default:0
+    memberDesc: {
+        type: String,
     },
-    memberArticles:{
-        type:Number,
-        default:0
+    memberProperties: {
+        type: Number,
+        default: 0
     },
-    memberFollowers:{
-        type:Number,
-        default:0
+    memberArticles: {
+        type: Number,
+        default: 0
     },
-    memberFollowings:{
-        type:Number,
-        default:0
+    memberFollowers: {
+        type: Number,
+        default: 0
     },
-    memberPoints:{
-        type:Number,
-        default:0
+    memberFollowings: {
+        type: Number,
+        default: 0
     },
-    memberLikes:{
-        type:Number,
-        default:0
+    memberPoints: {
+        type: Number,
+        default: 0
     },
-    memberViews:{
-        type:Number,
-        default:0
+    memberLikes: {
+        type: Number,
+        default: 0
     },
-    memberComments:{
-        type:Number,
-        default:0
+    memberViews: {
+        type: Number,
+        default: 0
     },
-    memberRank:{
-        type:Number,
-        default:0
+    memberComments: {
+        type: Number,
+        default: 0
     },
-    memberWarnings:{
-        type:Number,
-        default:0
+    memberRank: {
+        type: Number,
+        default: 0
     },
-    memberBlocks:{
-        type:Number,
-        default:0
+    memberWarnings: {
+        type: Number,
+        default: 0
     },
-    deletedAt:{
-        type:Date,
+    memberBlocks: {
+        type: Number,
+        default: 0
+    },
+    deletedAt: {
+        type: Date,
     }
-}, {timestamps:true,collection:"members"});
+}, { timestamps: true, collection: "members" });
 
 
 export default MemberSchema
