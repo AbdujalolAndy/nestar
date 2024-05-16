f901f411bdfcc6c9a2f5421cf663b625fd7e3902
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-05-17 02:51:37 +0900

    feat: Creation & verification Json Web Token

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from "bcryptjs"
+import { Member } from '../../libs/dto/member/member';
+import { T } from '../../libs/types/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+    constructor(private jsonService: JwtService) { }
+
     public async hashPassword(memberPassword: string): Promise<string> {
         const salt = await bcrypt.genSalt();
         return await bcrypt.hash(memberPassword, salt)
     }
 
-    public async comparePassord(password: string, hashedPassword): Promise<boolean> {
+    public async comparePassord(password: string, hashedPassword: string): Promise<boolean> {
         return await bcrypt.compare(password, hashedPassword)
+    }
+
+    public async createToken(member: Member): Promise<string> {
+        const payload: T = {};
+        Object.keys(member["_doc"] ? member["_doc"] : member).
+            map((ele) => payload[`${ele}`] = member[`${ele}`]);
+        delete payload.memberPassword;
+        return await this.jsonService.signAsync({
+            payload
+        })
+    }
+
+    public async verifyToken(token: string): Promise<Member> {
+        return this.jsonService.verifyAsync(token)
     }
 }
