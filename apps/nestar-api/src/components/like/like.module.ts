55be2d78472963b399140f57dcf183fa3aa21f3f
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-05-31 03:21:07 +0900

    feat: Create follow related modules & DTOs

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -4,7 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import LikeSchema from '../../schemas/Like.model';
 
 @Module({
-    imports: [MongooseModule.forFeature([{
+    imports: [
+        MongooseModule.forFeature([{
         name: "Like",
         schema: LikeSchema
     }])],
